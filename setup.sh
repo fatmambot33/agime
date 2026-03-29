8e51040707aefa83c3e7460edf7aec1ca95951d2
pfourca <51465707+pfourca@users.noreply.github.com>
2026-03-29 01:33:19 +0100

    new file:   setup.sh

---
+++ setup.sh
@@ -0,0 +1,27 @@
+sudo apt update && sudo apt upgrade -y
+//
+sudo apt install -y ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo usermod -aG docker $USER
+newgrp docker
+docker --version
+docker compose version
+docker ps
+
+
+docker network create proxy
+mkdir -p ~/docker/traefik && cd ~/docker/traefik
+nano docker-compose.yml
+
+
+mkdir letsencrypt
+touch letsencrypt/acme.json
+chmod 600 letsencrypt/acme.json
+
+docker compose up -d
